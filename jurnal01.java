4bbe967788939fa97ca3f362f9fd77f75323fb8f
RakhaArk12 <114908117+RakhaArk12@users.noreply.github.com>
2023-09-30 04:08:16 +0700

    Jurnal satu

---
+++ jurnal01.java
@@ -0,0 +1,13 @@
+public class jurnal01 {
+    public static void main(String[] args) {
+        // Mendeklarasikan variabel
+        String nama = "Rakha Arkan Azzahran";
+        String nim = "6706223078";
+        String kelas = "D3IF-47-01";
+
+        // Menampilkan informasi mahasiswa
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("Kelas : " + kelas);
+    }
+}
